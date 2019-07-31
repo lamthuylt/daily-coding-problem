79d297485e0b5b6390480c3ff85ac613b48a5725
PETIT-COUCOU\lamth <lamthuy.lt@gmail.com>
2019-07-31 21:48:45 +0200

    remove intermediate function

--- solutions/7_decode-message.py
+++ solutions/7_decode-message.py
@@ -10,12 +10,9 @@ You can assume that the messages are decodable. For example, '001' is not allowe
 
 def count_decode(message):
     nb_ways = 0
-    
-    def count_digits(message):
-        return len(message)
         
     def decode(message, nb_ways):
-        if count_digits(message) <= 2:
+        if len(message) <= 2:
             if message[0]!='0' and message>='1' and message<='10':  # '0' and 'Ox' are not decodable
                 nb_ways += 1
             elif message>='11' and message<='26':                   
@@ -24,7 +21,7 @@ def count_decode(message):
                 nb_ways += 1
             return nb_ways
 
-        elif count_digits(message) >= 3:
+        elif len(message) >= 3:
             first_digit = message[0]
             if first_digit!='0':
                 nb_ways = decode(message[1:], nb_ways)
@@ -44,6 +41,5 @@ if __name__ == "__main__":
     print(count_decode('12010'))
     
     
-
 
     
